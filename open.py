8f1383921e2fe1023a13b5df1a3e3b686f47e8e7
dxxo77 <3250668941@qq.com>
2026-08-08 22:52:18 +0800

    读取文件的操作，第一种要close 第二种with语法不需要close ps：补充写入的语法 附加是”a“  如果全部重写是”w“ psps:"r+"即可实现读入和附加的操作

--- open.py
+++ open.py
@@ -1,10 +1,14 @@
-f=open("D:/py项目/README.md","r", encoding="utf-8")
-print(f.read())
-f.close()
+# f=open("D:/py项目/README.md","r", encoding="utf-8")
+# print(f.read())
+# f.close()
 #or
 # 修正后完整代码
 # with open("D:/py项目/README.md","r", encoding="utf-8") as f:
 #     print(f.read())
 
-with open("D:/py项目/README.md","a", encoding="utf-8") as q:
-     q.write("(基础)")
+# with open("D:/py项目/README.md","a", encoding="utf-8") as q:
+#      q.write("(基础)")
+
+with open("D:/py项目/README.md", "r+", encoding="utf-8") as q:
+    print(q.read())
+    q.write("(基础)")
